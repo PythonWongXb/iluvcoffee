7ab91bb90c572381f413da330aa927100b25255e
Jim Strother <jim.strother@gmail.com>
2020-09-15 07:11:39 -0500

    added: Flavor entity
    there is currently an error: 'null value in column *name* violates not-null contranint', not sure where exactly it comes from

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -4,20 +4,27 @@ import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 @Injectable()
 export class CoffeesService {
   constructor(
     @InjectRepository(Coffee)
-    private readonly coffeeRepository: Repository<Coffee>
+    private readonly coffeeRepository: Repository<Coffee>,
+    @InjectRepository(Flavor)
+    private readonly flavorRepository: Repository<Flavor>,
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({
+      relations: ['flavors'],
+    });
   }
 
   async findOne(id: string) {
-    const coffee = await this.coffeeRepository.findOne(id);
+    const coffee = await this.coffeeRepository.findOne(id, {
+      relations: ['flavors'],
+    });
 
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found`);
@@ -26,15 +33,27 @@ export class CoffeesService {
     return coffee;
   }
 
-  create(createCoffeeDTO: CreateCoffeeDto) {
-    const coffee = this.coffeeRepository.create(createCoffeeDTO);
+  async create(createCoffeeDTO: CreateCoffeeDto) {
+    const flavors = await Promise.all(
+      createCoffeeDTO.flavors.map(name => this.preLoadFlavorByName(name)),
+    );
+    const coffee = this.coffeeRepository.create({
+      ...createCoffeeDTO,
+      flavors,
+    });
     return this.coffeeRepository.save(coffee);
   }
 
   async update(id: string, updateCoffeeDTO: UpdateCoffeeDto) {
+    const flavors = 
+      updateCoffeeDTO.flavors &&
+      (await Promise.all(
+        updateCoffeeDTO.flavors.map(name => this.preLoadFlavorByName(name)),
+      ));
     const coffee = await this.coffeeRepository.preload({
       id: +id,
       ...updateCoffeeDTO,
+      flavors,
     });
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found`);
@@ -45,5 +64,13 @@ export class CoffeesService {
   async remove(id: string) {
     const coffee = await this.coffeeRepository.findOne(id);
     return this.coffeeRepository.remove(coffee);
+  }
+
+  private async preLoadFlavorByName(name: string): Promise<Flavor> {
+    const existingFlavor = this.flavorRepository.findOne({ name });
+    if (existingFlavor) {
+      return existingFlavor;
+    }
+    return this.flavorRepository.create({ name });
   }
 }
