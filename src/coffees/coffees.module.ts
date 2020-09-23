edc96b112c9fdbc732abaf4414b1ffbbd404572a
Jim Strother <jim.strother@gmail.com>
2020-09-23 06:57:31 -0500

    added: anyc useFactory example

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Injectable, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Event } from '../events/entities/event.entity';
 import { CoffeesController } from './coffees.controller';
@@ -7,24 +7,22 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
-@Injectable()
-export class CoffeeBrandsFactory {
-  create() {
-    // do something
-    return ['buddy brew', 'nescafe'];
-  }
-}
+import { Connection } from 'typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    CoffeeBrandsFactory,
     {
       provide: COFFEE_BRANDS,
-      useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(),
-      inject: [CoffeeBrandsFactory],
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        // const coffeeBrands = await connection.query('SELECT * ...');
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+        console.log('Async Factory Function');
+        return coffeeBrands;
+      },
+      inject: [Connection]
     }
   ],
   exports: [CoffeesService],
