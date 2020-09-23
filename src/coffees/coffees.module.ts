ad507ec3a11f2c8a95101d144c7d441041e305c4
Jim Strother <jim.strother@gmail.com>
2020-09-23 06:48:18 -0500

    added: useFactory example

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Event } from '../events/entities/event.entity';
 import { CoffeesController } from './coffees.controller';
@@ -7,25 +7,24 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductionConfigService {}
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    // do something
+    return ['buddy brew', 'nescafe'];
+  }
+}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    {
-      provide: ConfigService,
-      useClass: 
-        process.env.NODE_ENV === 'development'
-          ? DevelopmentConfigService
-          : ProductionConfigService,
-    },
+    CoffeeBrandsFactory,
     {
       provide: COFFEE_BRANDS,
-      useValue: ['buddy brew', 'nescafe']
+      useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
     }
   ],
   exports: [CoffeesService],
